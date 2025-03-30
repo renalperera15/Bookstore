84dafd6754b550ee817816ecaa10ffe38df61931
Renal Perera <renalperera15@gmail.com>
2025-03-30 19:37:31 +0530

    Did some chnages to the cardresource

--- src/main/java/com/bookstore/resources/CartResource.java
+++ src/main/java/com/bookstore/resources/CartResource.java
@@ -1,11 +1,20 @@
 package com.bookstore.resources;
 
-import com.bookstore.models.Cart;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.*;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import com.bookstore.models.Cart;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/customers/{customerId}/cart")
 @Produces(MediaType.APPLICATION_JSON)
