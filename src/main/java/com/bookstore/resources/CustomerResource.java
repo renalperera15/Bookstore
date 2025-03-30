960adc9d126497086ee3b9837e12a56b83320bd9
dasuni <dasunisadeesha12@gmail.com>
2025-03-30 21:53:37 +0530

    added order resource

--- src/main/java/com/bookstore/resources/CustomerResource.java
+++ src/main/java/com/bookstore/resources/CustomerResource.java
@@ -1,4 +1,60 @@
 package com.bookstore.resources;
 
+import com.bookstore.models.Customer;
+import com.bookstore.storage.DataStore;
+import com.bookstore.exceptions.CustomerNotFoundException;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerResource {
+
+    @POST
+    public Response addCustomer(Customer customer) {
+        DataStore.addCustomer(customer);
+        return Response.status(Response.Status.CREATED).entity(customer).build();
+    }
+
+    @GET
+    public Response getAllCustomers() {
+        List<Customer> customers = DataStore.getAllCustomers();
+        return Response.ok(customers).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getCustomer(@PathParam("id") int id) {
+        Customer customer = DataStore.getCustomer(id);
+        if (customer == null) {
+            throw new CustomerNotFoundException("Customer not found with ID: " + id);
+        }
+        return Response.ok(customer).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateCustomer(@PathParam("id") int id, Customer updatedCustomer) {
+        Customer customer = DataStore.getCustomer(id);
+        if (customer == null) {
+            throw new CustomerNotFoundException("Customer not found with ID: " + id);
+        }
+        customer.setName(updatedCustomer.getName());
+        customer.setEmail(updatedCustomer.getEmail());
+        return Response.ok(customer).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteCustomer(@PathParam("id") int id) {
+        boolean removed = DataStore.removeCustomer(id);
+        if (!removed) {
+            throw new CustomerNotFoundException("Customer not found with ID: " + id);
+        }
+        return Response.noContent().build();
+    }
 }
