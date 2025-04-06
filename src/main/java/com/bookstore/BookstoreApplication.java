3565315f5ac2eee07b38255561945214d9c09d2f
Renal Perera <renalperera15@gmail.com>
2025-04-06 18:16:36 +0530

    Added a new bookstoreapplication

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,8 +1,28 @@
 package com.bookstore;
 
-import jakarta.ws.rs.ApplicationPath;
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
 public class BookstoreApplication extends Application {
+
+    public static final String BASE_URI = "http://localhost:8080/api/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("com.bookstore.resources");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("Bookstore API started at " + BASE_URI);
+        System.out.println("Press Enter to stop...");
+        System.in.read();
+        server.shutdownNow();
+    }
 }
