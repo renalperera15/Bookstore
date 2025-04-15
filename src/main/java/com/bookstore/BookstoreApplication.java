1871d2ca71dd9a05dcdefbdd09d32f18cd230b20
Renal Perera <renalperera15@gmail.com>
2025-04-15 13:21:36 +0530

    Added some part to the main code

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -8,13 +8,18 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 public class BookstoreApplication extends Application {
 
     public static final String BASE_URI = "http://localhost:8080/api/";
 
     public static HttpServer startServer() {
-        final ResourceConfig rc = new ResourceConfig().packages("com.bookstore.resources");
+        final ResourceConfig rc = new ResourceConfig()
+            .packages("com.bookstore.resources")
+            .register(GenericExceptionMapper.class); // Register the exception mapper
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
@@ -24,5 +29,15 @@ public class BookstoreApplication extends Application {
         System.out.println("Press Enter to stop...");
         System.in.read();
         server.shutdownNow();
+    }
+
+    @Provider
+    public static class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+        @Override
+        public Response toResponse(Throwable exception) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                           .entity("An unexpected error occurred: " + exception.getMessage())
+                           .build();
+        }
     }
 }
